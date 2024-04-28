4450b74db466b6d12f642760cda31a65ed181e78
Frans <frans_421_12@hotmail.com>
2024-04-27 20:38:43 -0500

    feat: Login design

--- src/app/components/inicio/login/login.component.ts
+++ src/app/components/inicio/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  login: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.login = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  log() {
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      password: this.login.value.password
+    }
+    console.log(usuario);
+  }
 }
