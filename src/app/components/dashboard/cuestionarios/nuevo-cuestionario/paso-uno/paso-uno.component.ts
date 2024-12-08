88e8cec1677018b1baaab59a5cc73f46d9890dd0
Frans <frans_421_12@hotmail.com>
2024-12-08 18:15:31 -0500

    feat: Cuestionario paso uno

--- src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-uno/paso-uno.component.ts
+++ src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-uno/paso-uno.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CuestionarioService } from 'src/app/services/cuestionario.service';
 
 @Component({
   selector: 'app-paso-uno',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./paso-uno.component.css']
 })
 export class PasoUnoComponent {
+  datosCuestionario: FormGroup;
 
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private cuestionarioService: CuestionarioService) {
+    this.datosCuestionario = this.fb.group({
+      titulo: ['', Validators.required],
+      descripcion: ['', Validators.required]
+    });
+  }
+
+  pasoUno(): void {
+    this.cuestionarioService.tituloCuestionario = this.datosCuestionario.value.titulo;
+    this.cuestionarioService.descripcionCuestionario = this.datosCuestionario.value.descripcion;
+    this.router.navigate(['/dashboard/nuevoCuestionario/pasoDos']);
+  }
 }
