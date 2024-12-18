6a5cfbc50bc9a743e1c4ab6b360e29820b2f9a19
Frans <frans_421_12@hotmail.com>
2024-12-17 23:46:30 -0500

    feat: Nueva Pregunta

--- src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/nueva-pregunta/nueva-pregunta.component.ts
+++ src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/nueva-pregunta/nueva-pregunta.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Pregunta } from 'src/app/models/pregunta';
+import { Respuesta } from 'src/app/models/respuesta';
 
 @Component({
   selector: 'app-nueva-pregunta',
@@ -6,5 +10,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./nueva-pregunta.component.css']
 })
 export class NuevaPreguntaComponent {
+  nuevaPregunta: FormGroup;
+  pregunta?: Pregunta;
+  rtaCorrecta = 0;
 
+  constructor(private fb: FormBuilder,
+              private toastr: ToastrService) {
+    this.nuevaPregunta = this.fb.group({
+      titulo: ['', Validators.required],
+      respuestas: this.fb.array([])
+    });
+  }
+
+  ngOnInit() {
+    this.agregarRespuestasPorDefecto();
+  }
+
+  get getRespuestas(): FormArray {
+    return this.nuevaPregunta.get('respuestas') as FormArray;
+  }
+
+  agregarRespuesta(): void {
+    this.getRespuestas.push(this.fb.group({
+      descripcion: ['', Validators.required],
+      esCorrecta: 0
+    }));
+  }
+
+  agregarRespuestasPorDefecto(): void {
+    this.agregarRespuesta();
+    this.agregarRespuesta();
+  }
+
+  eliminarRespuesta(index: number): void {
+    if (this.getRespuestas.length === 2) {
+      this.toastr.error('Como minimo la pregunta debe contener 2 respuestas', 'Error!');
+    } else {
+      this.getRespuestas.removeAt(index);
+    }
+  }
+
+  setRespuestaValida(index: number): void {
+    this.rtaCorrecta = index;
+  }
+
+  agregarPregunta(): void {
+    const descripcionPregunta = this.nuevaPregunta.get('titulo')?.value;
+    const arrayRespuestas = this.getRespuestas.value as { descripcion: string; esCorrecta: number }[];
+    const arrayRta: Respuesta[] = [];
+
+    arrayRespuestas.forEach((element, index) => {
+      const respuesta: Respuesta = new Respuesta(element.descripcion, false);
+      if (index === element.esCorrecta) {
+        respuesta.esCorrecta = true;
+      }
+      arrayRta.push(respuesta);
+    });
+
+    const pregunta: Pregunta = new Pregunta(descripcionPregunta, arrayRta);
+    console.log(pregunta);
+    this.reset();
+  }
+
+  reset(): void {
+    this.nuevaPregunta.reset();
+    this.getRespuestas.clear();
+    this.agregarRespuestasPorDefecto();
+  }
 }
